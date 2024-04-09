a1dd3db6883279c185011f1648ccbb66bae39f10
stephen-riggs <stephen.riggs@diamond.ac.uk>
2024-04-09 12:08:34 +0100

    Make numpy version same as cryolo

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     install_requires=[
         "mrcfile >=1.3.0",
         "Keras",
-        "numpy >= 1.16.0, < 1.26.4",
+        "numpy >= 1.16.0, <= 1.26.4",
         "h5py >= 2.5.0",
         "Pillow >= 6.0.0",
         "Cython",
