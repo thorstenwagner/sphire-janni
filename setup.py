8319f40e8d9e3e2139bd54c76241c160be94c984
thorstenwagner <twa1@posteo.de>
2024-04-09 11:50:51 +0200

    tf 1.15.4

--- setup.py
+++ setup.py
@@ -22,28 +22,28 @@ def find_version(*file_paths):
 setup(
     name='janni',
     version=find_version("janni", "__init__.py"),
-    python_requires='>3.4.0',
+    python_requires='>=3.5.0, <3.9',
     packages=['janni'],
     url='https://github.com/MPI-Dortmund/sphire-janni',
     license='MIT',
     author='Thorsten Wagner',
     setup_requires=["Cython"],
     extras_require={
-        'gpu': ['tensorflow-gpu == 1.10.1'],
-        'cpu': ['tensorflow == 1.10.1']
+        'gpu': ['tensorflow-gpu == 1.15.4'],
+        'cpu': ['tensorflow == 1.15.4']
     },
     install_requires=[
-        "mrcfile >=1.0.0,<= 1.1.2",
-        "Keras == 2.2.5",
-        "numpy == 1.14.5",
+        "mrcfile >=1.3.0",
+        "Keras == 2.3.1", # that doesnt seem to work. try tensorflow 1.14.0 in combination with keras 2.2.5
+        "numpy >= 1.16.0, < 1.19.0",
         "h5py >= 2.5.0, < 3.0.0",
         "Pillow >= 6.0.0",
         "Cython",
-        "imagecodecs-lite==2019.2.22",
-        "tifffile==2019.7.2",
-        "GooeyDev >= 1.0.3.2",
-        "wxPython == 4.0.4",
-        "scikit-image == 0.15.0"
+        "tifffile==2020.9.3",
+        "GooeyDev >= 1.0.8b3",
+        "wxPython >= 4.1.0",
+        "scikit-image >= 0.15.0",
+        "protobuf < 4"
     ],
     author_email='thorsten.wagner@mpi-dortmund.mpg.de',
     description='noise 2 noise for cryo em data',
